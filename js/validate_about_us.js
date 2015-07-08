8707c0bab542a956e351a41b5b2a62bc874fff8c
PedroBarroteSoares <pedrombsoares@outlook.pt>
2015-07-08 14:59:09 +0100

    f

--- js/validate_about_us.js
+++
@@ -1,9 +0,0 @@
-function validateForm_join_us() {
-    var name = document.forms["main-join_us-form"]["name"].value;
-	var institution = document
-	var text = 
-    if (name == null || name == "") {
-        alert("Introduza o nome");
-        return false;
-    }	
-}
